4b08c195cae3220cac9f4f73e99faa6999e219f6
cindy <cindy@163.com>
2016-12-02 15:33:18 +0800

    depot:add store

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,4 +71,8 @@ class ProductsController < ApplicationController
     def product_params
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
-end
+    end
+
+
+
+
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @update = {
+           title:  'Lorem Ipsum',
+           description: 'Wibbles are fun!',
+           image_url: 'lorem.jpg',
+           price: 19.95
+   }
   end
 
   test "should get index" do
@@ -17,10 +23,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    post :create, product: @update   
+#post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
     end
-
-    assert_redirected_to product_url(Product.last)
+    assert_redirected_to product_path(assigns(:product))
+#    assert_redirected_to product_url(Product.last)
   end
 
   test "should show product" do
@@ -34,7 +41,9 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+
+   put :update, id: @product, producti: @update 
+  # patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
     assert_redirected_to product_url(@product)
   end
 
